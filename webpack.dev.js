62b8ffdefd777ccacc8ffc0459001fac6a0e30dd
Aleksei-Uzin <aleksei.uzin@gmail.com>
2025-10-04 15:20:02 +0300

    feat: setup prettier and eslint

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const common = require('./webpack.common.js')
 const { merge } = require('webpack-merge')
 const path = require('path')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'development',
@@ -10,16 +11,17 @@ module.exports = merge(common, {
     hot: true,
     port: 9000,
     static: {
-      directory: path.join(__dirname, 'dist'),
-    },
+      directory: path.join(__dirname, 'dist')
+    }
   },
+  plugins: [new ReactRefreshWebpackPlugin()],
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
         sideEffects: true,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }
 })
